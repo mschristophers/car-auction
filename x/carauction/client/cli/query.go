20fb0e1b77f554d1b408e49c3585c133fa21f640
Christopher Simanjuntak <manuelstefan150@gmail.com>
2022-11-27 01:55:53 -0500

    Removed unnecessary stuffs

--- x/carauction/client/cli/query.go
+++ x/carauction/client/cli/query.go
@@ -25,15 +25,10 @@ func GetQueryCmd(queryRoute string) *cobra.Command {
 	}
 
 	cmd.AddCommand(CmdQueryParams())
-	cmd.AddCommand(CmdSellers())
 
-cmd.AddCommand(CmdAuctions())
+	cmd.AddCommand(CmdAuctions())
 
-cmd.AddCommand(CmdAuctions())
-
-cmd.AddCommand(CmdAuctions())
-
-// this line is used by starport scaffolding # 1
+	// this line is used by starport scaffolding # 1
 
 	return cmd
 }
